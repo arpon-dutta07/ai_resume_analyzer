6e69411cf0eea674ffd92838937860f8cc578af4
Arpon Dutta <arpongen3094@gmail.com>
2025-08-09 22:25:08 +0530

    #

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
-    { name: "description", content: "smart feedback for your home page" },
+    { name: "description", content: "smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Get a personalized resume rating and track the status of all your applications in one place.</h2>
+      </div>
+    </section>
+  </main>
 }
