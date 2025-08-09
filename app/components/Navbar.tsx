fb914a7e942c0f1b0ae8c025cf057eb01e233bbf
Arpon Dutta <arpongen3094@gmail.com>
2025-08-09 22:40:57 +0530

    #

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">
+      <p className="text-2xl font-bold text-gradient">
+        Resumind
+      </p>
+      </Link>
+    </nav>
+  )
+}
+
+export default Navbar
